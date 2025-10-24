3666d791103054537a5be7cb43b807807f17305d
eric- <ericl521@nycstudents.net>
2025-10-23 22:54:05 -0400

    Started working on DogWalker Challenge

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -1,4 +1,5 @@
 import  java.util.Scanner;
+
 public class DogWalkCompany {
     private int[] schedule;
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
@@ -13,6 +16,21 @@ public class Main {
         System.out.println(c);
         w = new DogWalker(3, c);
         System.out.println("$" + w.dogWalkingShift(7, 10) + ".00 earned");
+
+    }
+    public static int read() throws FileNotFoundException{
+        int money = 0;
+        DogWalkCompany[] companies = new DogWalkCompany[1000];
+        File f = new File("Companies.txt");
+        Scanner s = new Scanner(f);
+        int index = 0;
+        while(s.hasNextLine()){
+            int[] dogs = new int[24];
+            for (int i = 0; i < dogs.length; i++){
+                dogs[i] = s.nextInt();
+                companies[index] = new DogWalkCompany();
+            }
+        }
 
     }
 
